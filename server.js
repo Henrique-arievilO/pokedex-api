0a08108765c3c20452cd139bc51f847116261724
Henrique Oliveira <wandholiveira1@gmail.com>
2025-04-16 13:39:28 -0300

    Adiciona suporte ao CORS na API para acesso via front end

--- server.js
+++ server.js
@@ -3,8 +3,17 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
 
-// Instrui o Express a utilizar seu parser interno para JSON
+// Midleware instrui o Express a utilizar seu parser interno para JSON
 app.use(express.json());
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE, OPTIONS");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
+const cors = require('cors');
+app.use(cors());
 
 //BASE ENDPOINT
 // Endpoint básico: raiz da API
