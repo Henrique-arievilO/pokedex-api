1886935efb52a07743ba911e95f38bfba34ce0d6
Henrique Oliveira <wandholiveira1@gmail.com>
2025-05-06 01:31:18 -0300

    :artist_palette: style: ajustes na personalização do Swagger

--- server.js
+++ server.js
@@ -6,19 +6,26 @@ import pokemonRoutes from './src/routes/pokemonRoutes.js';
 import swaggerJsdoc from 'swagger-jsdoc';
 import swaggerUi from 'swagger-ui-express';
 import swaggerOptions from './config/swagger/swaggerOptions.js';
+import fs from 'fs';
+import path from 'path';
+import { fileURLToPath } from 'url';
 
 dotenv.config();
 
 //Variáveis
 const app = express();
 const PORT = process.env.PORT || 3000;
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 //Middlewares
 app.use(express.json());
 app.use('/api', pokemonRoutes);
+app.use(express.static(path.join(__dirname, 'public')));
 
+const customCss = fs.readFileSync(path.join(__dirname, 'public/css/swagger-custom.css'), 'utf8');
 const specs = swaggerJsdoc(swaggerOptions);
-app.use('/docs', swaggerUi.serve, swaggerUi.setup(specs));
+app.use('/docs', swaggerUi.serve, swaggerUi.setup(specs, { customCss }));
 
 const startServer = async () => {
   //Funções
